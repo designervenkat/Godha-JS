0747826726e7013bba2eba25c37b9390cff5b60f
@designervenkat <murty.av@gmail.com>
2025-04-30 01:31:05 +0000

    Functions Part One

---
+++ JS-09/main.js
@@ -0,0 +1,49 @@
+// Define a functions
+
+// Function Declartion // Named Function
+
+// function greetingToUser(username) {
+//     if (!username) {
+//         console.log('Please enter valid username')
+//         return
+//     }
+//     console.log(`${username}, Hi there`)
+// }
+
+// greetingToUser('Sam')
+
+// let num1 = 4
+// let num2 = 2
+
+// function addTwo(num1, num2) {
+//     let result = num1 + num2
+//     return result
+// }
+
+// const result = addTwo(4, 'a')
+// console.log(result)
+
+// function multiply(result) {
+//     if (isNaN(result)) {
+//         // Use isNaN to check if the value is NaN
+//         console.log('Please enter valid results')
+//         return
+//     }
+//     console.log(result * 2)
+// }
+
+// multiply(result)
+
+// Function Expression / Unamed function // anonymous function
+
+// let greetingToAdmin = function (message) {
+//     console.log(message)
+// }
+
+// console.log(typeof greetingToAdmin)
+
+// IIFE Function
+
+// ;(function (msg) {
+//     console.log(msg)
+// })('Hello there')
