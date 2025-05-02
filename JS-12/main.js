ca5e4a7514fd6d5917d242748aa77aa0c4001dcb
@designervenkat <murty.av@gmail.com>
2025-05-02 03:03:15 +0000

    Arrow Function

---
+++ JS-12/main.js
@@ -0,0 +1,72 @@
+// Arrow Function Experssion
+
+// function addOne(name) {
+//     console.log(`Hello ${name}`)
+// }
+
+// addOne('John')
+
+// THIS - give you the
+
+// const itemsInCart = {
+//     id: 1234,
+//     username: 'John',
+//     price: 999.99,
+
+//     welcomeMessage: function () {
+//         console.log(`Thank you for purchase! ${this.username}`)
+//         console.log(this)
+//     },
+// }
+
+// itemsInCart.welcomeMessage()
+// itemsInCart.username = 'Sam'
+// itemsInCart.welcomeMessage()
+
+// console.log(this)
+
+// function one() {
+//     let username = 'john'
+//     console.log(this.username)
+//     // console.log(this)
+// }
+// one()
+
+// Arrow function exp.
+
+// const two = () => {
+//     let username = 'john'
+//     // console.log(this)
+//     console.log(this.username)
+// }
+// two()
+
+// Readable, simple and one-liner syntax
+// function and return keyword can be removed
+
+// const sum = (num1, num2) => {
+//     console.log(num1 + num2)
+//     return num1 + num2
+// }
+// console.log(sum(4, 4))
+
+// const sum = (num1, num2) => num1 + num2
+
+// const user =
+
+// const items = () => ({ username: 'john', id: 1234 })
+// const items = () => {
+//     return [1, 2, 3]
+// }
+
+// console.log(items())
+
+// Immediately Invoked Function Experssion
+
+// ;(function () {
+//     console.log(`Database is connected `)
+// })()
+
+// ;(() => {
+//     console.log(`Database is connected Two`)
+// })()
